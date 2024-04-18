c4dbe3da7c994114b78515278fc8e0eb17060dba
Bryson Oscar <brysonosc20@gmail.com>
2024-04-18 18:13:32 +0300

    Initial commit message for base POS

---
+++ app/config/config.php
@@ -0,0 +1,22 @@
+<?php
+// setting time zone
+date_default_timezone_set("Asia/Kolkata");
+
+// define the site root
+// define('SITE_ROOT', 'https://localhost/wordpress/PHP-Point-Of-Sale'); # changed to resolve Mixed Content error
+define('SITE_ROOT', 'http://localhost/wordpress/PHP-Point-Of-Sale');
+
+
+
+// Database Information
+// Database Hostname
+define('DATABASE_HOST','localhost');
+// Database Username
+define('DATABASE_USER','root');
+// Database Name
+define('DATABASE_NAME', 'ample');
+// Database DB_PASS
+define('DATABASE_PASS','');
+
+
+ ?>
